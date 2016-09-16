c5ac44f7981c9467c9ba41a8fb089a7d65dfb412
Maksim Rakitin <maxim.rakitin@gmail.com>
2016-09-16 17:41:16 +0000

    Mask optical element added to SRW.

---
+++ env/work/srw_python/SRWLIB_Example17.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+try:
+    __IPYTHON__
+    import sys
+    del sys.argv[1:]
+except:
+    pass
+
+from srwl_bl import *
+import srwlib
+
+
+def set_optics(v=None):
+    el = []
+    el.append(srwlib.SRWLOptMirEl(_p=50.0, _q=0.4, _ang_graz=0.003, _size_tang=0.2, _size_sag=0.01, _nvx=0.0, _nvy=0.999995500003, _nvz=-0.0029999955, _tvx=0.0, _tvy=-0.0029999955))
+    el.append(srwlib.SRWLOptD(0.2))
+    el.append(srwlib.SRWLOptMirEl(_p=50.0, _q=0.2, _ang_graz=0.003, _size_tang=0.2, _size_sag=0.01, _nvx=0.999995500003, _nvy=0.0, _nvz=-0.0029999955, _tvx=-0.0029999955, _tvy=0.0))
+    el.append(srwlib.SRWLOptD(0.4))
+    el.append(srwlib.srwl_opt_setup_mask(_delta=1, _atten_len=1, _thick=1, _grid_sh=0, _grid_dx=5.e-06,
+                             _grid_dy=5.e-06, _pitch_x=20.e-6, _pitch_y=20.e-6, _grid_nx=21,
+                             _grid_ny=21, _mask_Nx=1024, _mask_Ny=1024, _grid_angle=0.4363323129985824,
+                             _hx=7.32e-07, _hy=7.32e-07,_mask_x0=0, _mask_y0=0))
+    el.append(srwlib.SRWLOptD(0.2))
+
+    pp = []
+    pp.append([0, 0, 1.0, 0, 0, 1.0, 1.0, 1.0, 1.0])#mirror
+    pp.append([0, 0, 1.0, 1, 0, 1.0, 1.0, 1.0, 1.0])#drift
+
+    pp.append([0, 0, 1.0, 0, 0, 1.0, 1.0, 1.0, 1.0])#mirror
+    pp.append([0, 0, 1.0, 1, 0, 1.0, 1.0, 1.0, 1.0])#drift
+
+    pp.append([0, 0, 1.0, 0, 0, 1.0, 1.0, 1.0, 1.0])#mask
+    pp.append([0, 0, 1.0, 1, 0, 1.0, 1.0, 1.0, 1.0])#drift
+
+    pp.append([0, 0, 1.0, 0, 0, 1.0, 1.0, 1.0, 1.0])#final
+
+    return srwlib.SRWLOptC(el, pp)
+
+
+varParam = srwl_uti_ext_options([
+    ['name', 's', 'test', 'simulation name'],
+
+#---Data Folder
+    ['fdir', 's', 'data_example_17', 'folder (directory) name for reading-in input and saving output data files'],
+
+
+    ['gbm_x', 'f', 0.0, 'average horizontal coordinates of waist [m]'],
+    ['gbm_y', 'f', 0.0, 'average vertical coordinates of waist [m]'],
+    ['gbm_z', 'f', 0.0, 'average longitudinal coordinate of waist [m]'],
+    ['gbm_xp', 'f', 0.0, 'average horizontal angle at waist [rad]'],
+    ['gbm_yp', 'f', 0.0, 'average verical angle at waist [rad]'],
+    ['gbm_ave', 'f', 9000.0, 'average photon energy [eV]'],
+    ['gbm_pen', 'f', 0.001, 'energy per pulse [J]'],
+    ['gbm_rep', 'f', 1, 'rep. rate [Hz]'],
+    ['gbm_pol', 'f', 1, 'polarization 1- lin. hor., 2- lin. vert., 3- lin. 45 deg., 4- lin.135 deg., 5- circ. right, 6- circ. left'],
+    ['gbm_sx', 'f', 3e-06, 'rms beam size vs horizontal position [m] at waist (for intensity)'],
+    ['gbm_sy', 'f', 3e-06, 'rms beam size vs vertical position [m] at waist (for intensity)'],
+    ['gbm_st', 'f', 1e-13, 'rms pulse duration [s] (for intensity)'],
+    ['gbm_mx', 'f', 0, 'transverse Gauss-Hermite mode order in horizontal direction'],
+    ['gbm_my', 'f', 0, 'transverse Gauss-Hermite mode order in vertical direction'],
+    ['gbm_ca', 's', 'c', 'treat _sigX, _sigY as sizes in [m] in coordinate representation (_presCA="c") or as angular divergences in [rad] in angular representation (_presCA="a")'],
+    ['gbm_ft', 's', 't', 'treat _sigT as pulse duration in [s] in time domain/representation (_presFT="t") or as bandwidth in [eV] in frequency domain/representation (_presFT="f")'],
+
+#---Calculation Types
+    # Electron Trajectory
+    ['tr', '', '', 'calculate electron trajectory', 'store_true'],
+    ['tr_cti', 'f', 0.0, 'initial time moment (c*t) for electron trajectory calculation [m]'],
+    ['tr_ctf', 'f', 0.0, 'final time moment (c*t) for electron trajectory calculation [m]'],
+    ['tr_np', 'f', 10000, 'number of points for trajectory calculation'],
+    ['tr_mag', 'i', 1, 'magnetic field to be used for trajectory calculation: 1- approximate, 2- accurate'],
+    ['tr_fn', 's', 'res_trj.dat', 'file name for saving calculated trajectory data'],
+    ['tr_pl', 's', '', 'plot the resulting trajectiry in graph(s): ""- dont plot, otherwise the string should list the trajectory components to plot'],
+
+    #Single-Electron Spectrum vs Photon Energy
+    ['ss', '', '', 'calculate single-e spectrum vs photon energy', 'store_true'],
+    ['ss_ei', 'f', 100.0, 'initial photon energy [eV] for single-e spectrum vs photon energy calculation'],
+    ['ss_ef', 'f', 20000.0, 'final photon energy [eV] for single-e spectrum vs photon energy calculation'],
+    ['ss_ne', 'i', 10000, 'number of points vs photon energy for single-e spectrum vs photon energy calculation'],
+    ['ss_x', 'f', 0.0, 'horizontal position [m] for single-e spectrum vs photon energy calculation'],
+    ['ss_y', 'f', 0.0, 'vertical position [m] for single-e spectrum vs photon energy calculation'],
+    ['ss_meth', 'i', 1, 'method to use for single-e spectrum vs photon energy calculation: 0- "manual", 1- "auto-undulator", 2- "auto-wiggler"'],
+    ['ss_prec', 'f', 0.01, 'relative precision for single-e spectrum vs photon energy calculation (nominal value is 0.01)'],
+    ['ss_pol', 'i', 6, 'polarization component to extract after spectrum vs photon energy calculation: 0- Linear Horizontal, 1- Linear Vertical, 2- Linear 45 degrees, 3- Linear 135 degrees, 4- Circular Right, 5- Circular Left, 6- Total'],
+    ['ss_mag', 'i', 1, 'magnetic field to be used for single-e spectrum vs photon energy calculation: 1- approximate, 2- accurate'],
+    ['ss_ft', 's', 'f', 'presentation/domain: "f"- frequency (photon energy), "t"- time'],
+    ['ss_u', 'i', 1, 'electric field units: 0- arbitrary, 1- sqrt(Phot/s/0.1%bw/mm^2), 2- sqrt(J/eV/mm^2) or sqrt(W/mm^2), depending on representation (freq. or time)'],
+    ['ss_fn', 's', 'res_spec_se.dat', 'file name for saving calculated single-e spectrum vs photon energy'],
+    ['ss_pl', 's', '', 'plot the resulting single-e spectrum in a graph: ""- dont plot, "e"- show plot vs photon energy'],
+
+    #Multi-Electron Spectrum vs Photon Energy (taking into account e-beam emittance, energy spread and collection aperture size)
+    ['sm', '', '', 'calculate multi-e spectrum vs photon energy', 'store_true'],
+    ['sm_ei', 'f', 100.0, 'initial photon energy [eV] for multi-e spectrum vs photon energy calculation'],
+    ['sm_ef', 'f', 20000.0, 'final photon energy [eV] for multi-e spectrum vs photon energy calculation'],
+    ['sm_ne', 'i', 10000, 'number of points vs photon energy for multi-e spectrum vs photon energy calculation'],
+    ['sm_x', 'f', 0.0, 'horizontal center position [m] for multi-e spectrum vs photon energy calculation'],
+    ['sm_rx', 'f', 0.001, 'range of horizontal position / horizontal aperture size [m] for multi-e spectrum vs photon energy calculation'],
+    ['sm_nx', 'i', 1, 'number of points vs horizontal position for multi-e spectrum vs photon energy calculation'],
+    ['sm_y', 'f', 0.0, 'vertical center position [m] for multi-e spectrum vs photon energy calculation'],
+    ['sm_ry', 'f', 0.001, 'range of vertical position / vertical aperture size [m] for multi-e spectrum vs photon energy calculation'],
+    ['sm_ny', 'i', 1, 'number of points vs vertical position for multi-e spectrum vs photon energy calculation'],
+    ['sm_mag', 'i', 1, 'magnetic field to be used for calculation of multi-e spectrum spectrum or intensity distribution: 1- approximate, 2- accurate'],
+    ['sm_hi', 'i', 1, 'initial UR spectral harmonic to be taken into account for multi-e spectrum vs photon energy calculation'],
+    ['sm_hf', 'i', 15, 'final UR spectral harmonic to be taken into account for multi-e spectrum vs photon energy calculation'],
+    ['sm_prl', 'f', 1.0, 'longitudinal integration precision parameter for multi-e spectrum vs photon energy calculation'],
+    ['sm_pra', 'f', 1.0, 'azimuthal integration precision parameter for multi-e spectrum vs photon energy calculation'],
+    ['sm_meth', 'i', -1, 'method to use for spectrum vs photon energy calculation in case of arbitrary input magnetic field: 0- "manual", 1- "auto-undulator", 2- "auto-wiggler", -1- dont use this accurate integration method (rather use approximate if possible)'],
+    ['sm_prec', 'f', 0.01, 'relative precision for spectrum vs photon energy calculation in case of arbitrary input magnetic field (nominal value is 0.01)'],
+    ['sm_nm', 'i', 1, 'number of macro-electrons for calculation of spectrum in case of arbitrary input magnetic field'],
+    ['sm_na', 'i', 5, 'number of macro-electrons to average on each node at parallel (MPI-based) calculation of spectrum in case of arbitrary input magnetic field'],
+    ['sm_ns', 'i', 5, 'saving periodicity (in terms of macro-electrons) for intermediate intensity at calculation of multi-electron spectrum in case of arbitrary input magnetic field'],
+    ['sm_type', 'i', 1, 'calculate flux (=1) or flux per unit surface (=2)'],
+    ['sm_pol', 'i', 6, 'polarization component to extract after calculation of multi-e flux or intensity: 0- Linear Horizontal, 1- Linear Vertical, 2- Linear 45 degrees, 3- Linear 135 degrees, 4- Circular Right, 5- Circular Left, 6- Total'],
+    ['sm_rm', 'i', 1, 'method for generation of pseudo-random numbers for e-beam phase-space integration: 1- standard pseudo-random number generator, 2- Halton sequences, 3- LPtau sequences (to be implemented)'],
+    ['sm_fn', 's', 'res_spec_me.dat', 'file name for saving calculated milti-e spectrum vs photon energy'],
+    ['sm_pl', 's', '', 'plot the resulting spectrum-e spectrum in a graph: ""- dont plot, "e"- show plot vs photon energy'],
+    #to add options for the multi-e calculation from "accurate" magnetic field
+
+    #Power Density Distribution vs horizontal and vertical position
+    ['pw', '', '', 'calculate SR power density distribution', 'store_true'],
+    ['pw_x', 'f', 0.0, 'central horizontal position [m] for calculation of power density distribution vs horizontal and vertical position'],
+    ['pw_rx', 'f', 0.015, 'range of horizontal position [m] for calculation of power density distribution vs horizontal and vertical position'],
+    ['pw_nx', 'i', 100, 'number of points vs horizontal position for calculation of power density distribution'],
+    ['pw_y', 'f', 0.0, 'central vertical position [m] for calculation of power density distribution vs horizontal and vertical position'],
+    ['pw_ry', 'f', 0.015, 'range of vertical position [m] for calculation of power density distribution vs horizontal and vertical position'],
+    ['pw_ny', 'i', 100, 'number of points vs vertical position for calculation of power density distribution'],
+    ['pw_pr', 'f', 1.0, 'precision factor for calculation of power density distribution'],
+    ['pw_meth', 'i', 1, 'power density computation method (1- "near field", 2- "far field")'],
+    ['pw_zst', 'f', 0., 'initial longitudinal position along electron trajectory of power density distribution (effective if pow_sst < pow_sfi)'],
+    ['pw_zfi', 'f', 0., 'final longitudinal position along electron trajectory of power density distribution (effective if pow_sst < pow_sfi)'],
+    ['pw_mag', 'i', 1, 'magnetic field to be used for power density calculation: 1- approximate, 2- accurate'],
+    ['pw_fn', 's', 'res_pow.dat', 'file name for saving calculated power density distribution'],
+    ['pw_pl', 's', '', 'plot the resulting power density distribution in a graph: ""- dont plot, "x"- vs horizontal position, "y"- vs vertical position, "xy"- vs horizontal and vertical position'],
+
+    #Single-Electron Intensity distribution vs horizontal and vertical position
+    ['si', '', '', 'calculate single-e intensity distribution (without wavefront propagation through a beamline) vs horizontal and vertical position', 'store_true'],
+    #Single-Electron Wavefront Propagation
+    ['ws', '', '', 'calculate single-electron (/ fully coherent) wavefront propagation', 'store_true'],
+    #Multi-Electron (partially-coherent) Wavefront Propagation
+    ['wm', '', '', 'calculate multi-electron (/ partially coherent) wavefront propagation', 'store_true'],
+
+    ['w_e', 'f', 9000.0, 'photon energy [eV] for calculation of intensity distribution vs horizontal and vertical position'],
+    ['w_ef', 'f', -1., 'final photon energy [eV] for calculation of intensity distribution vs horizontal and vertical position'],
+    ['w_ne', 'i', 1, 'number of points vs photon energy for calculation of intensity distribution'],
+    ['w_x', 'f', 0.0, 'central horizontal position [m] for calculation of intensity distribution'],
+    ['w_rx', 'f', 0.002, 'range of horizontal position [m] for calculation of intensity distribution'],
+    ['w_nx', 'i', 2048, 'number of points vs horizontal position for calculation of intensity distribution'],
+    ['w_y', 'f', 0.0, 'central vertical position [m] for calculation of intensity distribution vs horizontal and vertical position'],
+    ['w_ry', 'f', 0.002, 'range of vertical position [m] for calculation of intensity distribution vs horizontal and vertical position'],
+    ['w_ny', 'i', 2048, 'number of points vs vertical position for calculation of intensity distribution'],
+    ['w_smpf', 'f', 0, 'sampling factor for calculation of intensity distribution vs horizontal and vertical position'],
+    ['w_meth', 'i', 2, 'method to use for calculation of intensity distribution vs horizontal and vertical position'],
+    ['w_prec', 'f', 0.01, 'relative precision for calculation of intensity distribution vs horizontal and vertical position'],
+    ['w_u', 'i', 1, 'electric field units: 0- arbitrary, 1- sqrt(Phot/s/0.1%bw/mm^2), 2- sqrt(J/eV/mm^2) or sqrt(W/mm^2), depending on representation (freq. or time)'],
+    ['si_pol', 'i', 6, 'polarization component to extract after calculation of intensity distribution: 0- Linear Horizontal, 1- Linear Vertical, 2- Linear 45 degrees, 3- Linear 135 degrees, 4- Circular Right, 5- Circular Left, 6- Total'],
+    ['si_type', 'i', 0, 'type of a characteristic to be extracted after calculation of intensity distribution: 0- Single-Electron Intensity, 1- Multi-Electron Intensity, 2- Single-Electron Flux, 3- Multi-Electron Flux, 4- Single-Electron Radiation Phase, 5- Re(E): Real part of Single-Electron Electric Field, 6- Im(E): Imaginary part of Single-Electron Electric Field, 7- Single-Electron Intensity, integrated over Time or Photon Energy'],
+    ['w_mag', 'i', 1, 'magnetic field to be used for calculation of intensity distribution vs horizontal and vertical position: 1- approximate, 2- accurate'],
+
+    ['si_fn', 's', 'res_int_se.dat', 'file name for saving calculated single-e intensity distribution (without wavefront propagation through a beamline) vs horizontal and vertical position'],
+    ['si_pl', 's', '', 'plot the input intensity distributions in graph(s): ""- dont plot, "x"- vs horizontal position, "y"- vs vertical position, "xy"- vs horizontal and vertical position'],
+    ['ws_fni', 's', 'res_int_pr_se.dat', 'file name for saving propagated single-e intensity distribution vs horizontal and vertical position'],
+    ['ws_pl', 's', '', 'plot the resulting intensity distributions in graph(s): ""- dont plot, "x"- vs horizontal position, "y"- vs vertical position, "xy"- vs horizontal and vertical position'],
+
+    ['wm_nm', 'i', 100000, 'number of macro-electrons (coherent wavefronts) for calculation of multi-electron wavefront propagation'],
+    ['wm_na', 'i', 5, 'number of macro-electrons (coherent wavefronts) to average on each node for parallel (MPI-based) calculation of multi-electron wavefront propagation'],
+    ['wm_ns', 'i', 5, 'saving periodicity (in terms of macro-electrons / coherent wavefronts) for intermediate intensity at multi-electron wavefront propagation calculation'],
+    ['wm_ch', 'i', 0, 'type of a characteristic to be extracted after calculation of multi-electron wavefront propagation: #0- intensity (s0); 1- four Stokes components; 2- mutual intensity cut vs x; 3- mutual intensity cut vs y'],
+    ['wm_ap', 'i', 0, 'switch specifying representation of the resulting Stokes parameters: coordinate (0) or angular (1)'],
+    ['wm_x0', 'f', 0, 'horizontal center position for mutual intensity cut calculation'],
+    ['wm_y0', 'f', 0, 'vertical center position for mutual intensity cut calculation'],
+    ['wm_ei', 'i', 0, 'integration over photon energy is required (1) or not (0); if the integration is required, the limits are taken from w_e, w_ef'],
+    ['wm_rm', 'i', 1, 'method for generation of pseudo-random numbers for e-beam phase-space integration: 1- standard pseudo-random number generator, 2- Halton sequences, 3- LPtau sequences (to be implemented)'],
+    ['wm_fni', 's', 'res_int_pr_me.dat', 'file name for saving propagated multi-e intensity distribution vs horizontal and vertical position'],
+
+    #to add options
+    ['op_r', 'f', 50.0, 'longitudinal position of the first optical element [m]'],
+
+    # Former appParam:
+    ['source_type', 's', 'g', 'source type, (u) idealized undulator, (t), tabulated undulator, (m) multipole, (g) gaussian beam'],
+
+])
+
+
+def main():
+    v = srwl_uti_parse_options(varParam)
+    source_type, mag = setup_source(v)
+    op = set_optics()
+    v.ws = True
+    v.ws_pl = 'xy'
+    SRWLBeamline(_name=v.name, _mag_approx=mag).calc_all(v, op)
+
+
+if __name__ == '__main__':
+    main()
--- env/work/srw_python/srwlib.py
+++ env/work/srw_python/srwlib.py
@@ -2908,6 +2908,141 @@ def srwl_opt_setup_cyl_fiber(_foc_plane, _delta_ext, _delta_core, _atten_len_ext
     return opT
 
 #****************************************************************************
+def srwl_opt_setup_mask(_delta, _atten_len, _thick,
+                        _hx, _hy, _pitch_x, _pitch_y, _mask_Nx, _mask_Ny,
+                        _grid_nx, _grid_ny, _grid_sh, _grid_dx, _grid_dy=0, _grid_angle=0, _mask_x0=0, _mask_y0=0):
+    """Setup Transmission type Optical Element which simulates a mask array for at-wavelength metrology.
+
+    :param _delta: refractive index decrement (can be one number of array vs photon energy)
+    :param _atten_len: attenuation length [m] (can be one number of array vs photon energy)
+    :param _thick: thickness of mask [m]
+    :param _hx: sampling interval in x-direction [m]
+    :param _hy: sampling interval in y-direction [m]
+    :param _pitch_x: grid pitch in x-direction [m]
+    :param _pitch_y: grid pitch in y-direction [m]
+    :param _mask_Nx: number of pixels in x-direction [1]
+    :param _mask_Ny: number of pixels in y-direction [1]
+    :param _grid_nx: number of grids in x-direction
+    :param _grid_ny: number of grids in y-direction
+    :param _grid_sh: grid shape (0: Circular grids case. 1: Rectangular grids case. 2: 2-D phase grating)
+    :param _grid_dx: grid dimension in x-direction, width for rectangular or elliptical grids [m]
+    :param _grid_dy: grid dimension in y-direction, height for rectangular or elliptical grids [m]
+    :param _grid_angle: tilt angle of the grid [rad]
+    :param _mask_x0: horizontal coordinate of the mask [m]
+    :param _mask_y0: vertical coordinate of the mask [m]
+    :return: transmission (SRWLOptT) type optical element which simulates the PMA
+    """
+    # Check if _grid_dy is set by user.
+    if _grid_dy == 0:
+        _grid_dy = _grid_dx  # An ellipse becomes a circle and a rectangle becomes a square.
+    if _grid_sh == 2:
+        _grid_dx = _pitch_x  # Change grid_size for 2D grating, grid_size equal to pitch
+        _grid_dy = _pitch_y
+    # Calculate the range of mask.
+    mask_Rx = _hx * _mask_Nx  # mask range in x-direction [m].
+    mask_Ry = _hy * _mask_Nx  # mask range in y-direction [m].
+
+    # Calculate the range of grid.
+    grid_Rx = _pitch_x * _grid_nx  # grid range in x-direction [m].
+    grid_Ry = _pitch_y * _grid_ny  # grid range in y-direction [m].
+
+    # Generate Transmission Optical Element.
+    trans_opt = SRWLOptT(_nx=_mask_Nx, _ny=_mask_Ny, _rx=mask_Rx, _ry=mask_Ry, _arTr=None, _extTr=0, _x=0, _y=0)
+
+    # Same data alignment as for wavefront: outer loop vs y, inner loop vs x.
+    pointer = 0  # pointer for array trans_opt.arTr
+    y = - mask_Ry / 2  # Mask is always centered on the grid, however grid can be shifted.
+
+    for iy in range(_mask_Ny):
+        # Calculate the relative position in y.
+        # NOTE: Use round to solve the precision issue!
+        pitch_num_y = floor(round(y / _pitch_y, 9))
+        y_rel = y - (pitch_num_y * _pitch_y) - _mask_y0
+        if y_rel >= _pitch_y / 2:
+            y_rel -= _pitch_y
+
+        x = - mask_Rx / 2  # Mask is always centered on the grid, however grid can be shifted.
+        for ix in range(_mask_Nx):
+            # Calculate the relative position in x.
+            # NOTE: Use round to solve the precision issue!
+            pitch_num_x = floor(round(x / _pitch_x, 9))
+            x_rel = x - (pitch_num_x * _pitch_x) - _mask_x0
+
+            if x_rel >= _pitch_x / 2:
+                x_rel -= _pitch_x
+
+            # Initialize the bool parameter.
+            inside_hole = False
+            phase_shift = False
+
+            # Hartmann hole in an elliptical shape.
+            if _grid_sh == 0:
+                if (x_rel / _grid_dx) ** 2 + (y_rel / _grid_dy) ** 2 < 1 \
+                        and not (round(x_rel - (x - _mask_x0), 9) == 0 and round(y_rel - (y - _mask_y0), 9) == 0) \
+                        and abs(x) < grid_Rx / 2 and abs(y) < grid_Ry / 2:
+                    inside_hole = True
+
+            # Hartmann hole in a rectangular shape.
+            elif _grid_sh == 1:
+                # Calculate the equations for edges of rectangle.
+                xCross1 = - _grid_dx / (2 ** 0.5) * math.cos(_grid_angle)
+                yCross1 = - _grid_dx / (2 ** 0.5) * math.sin(_grid_angle)
+                xCross2 = + _grid_dx / (2 ** 0.5) * math.cos(_grid_angle)
+                yCross2 = + _grid_dx / (2 ** 0.5) * math.sin(_grid_angle)
+                k1 = math.tan(math.pi / 4 + _grid_angle)
+                k2 = -math.tan(math.pi / 4 - _grid_angle)
+                k4 = math.tan(math.pi / 4 + _grid_angle)
+                k3 = -math.tan(math.pi / 4 - _grid_angle)
+
+                if (k2 * x_rel + (yCross2 - k2 * xCross2)) > y_rel > (k3 * x_rel + (yCross1 - k3 * xCross1)) \
+                        and (k1 * x_rel + (yCross1 - k1 * xCross1)) > y_rel > (k4 * x_rel + (yCross2 - k4 * xCross2)) \
+                        and not (abs(x - _mask_x0) < _pitch_x / 2 and abs(y - _mask_y0) < _pitch_y / 2) \
+                        and abs(x) < grid_Rx / 2 and abs(y) < grid_Ry / 2:
+                    inside_hole = True
+
+            # Grating shearing interferometry in a 2D phase grating.
+            elif _grid_sh == 2:
+                phase_shift = False
+                if (x_rel >= 0 and y_rel < 0) or (x_rel < 0 and y_rel >= 0):
+                    phase_shift = True
+
+            else:
+                raise ValueError('Unknown shape code.')
+
+            # Give values to trans_opt.arTr.
+            if inside_hole and not (_grid_sh == 2):
+                trans_opt.arTr[pointer] = 1  # amplitude transmission.  (!) not in physics yet
+                trans_opt.arTr[pointer + 1] = 0  # optical path difference. (!) not in physics yet
+            else:
+                trans_opt.arTr[pointer] = 0  # amplitude transmission.  (!) not in physics yet
+                trans_opt.arTr[pointer + 1] = 0  # optical path difference. (!) not in physics yet
+
+            if _grid_sh == 2:
+                # Give values to OpT.arTr
+                # Make final judgement.
+                if phase_shift:
+                    trans_opt.arTr[pointer] = exp(
+                        -0.5 * _thick / _atten_len)  # amplitude transmission.
+                    trans_opt.arTr[pointer + 1] = -_delta * _thick  # optical path difference.
+                else:
+                    trans_opt.arTr[pointer] = 1  # amplitude transmission.
+                    trans_opt.arTr[pointer + 1] = 0  # optical path difference.
+                if not (abs(x) < grid_Rx / 2 and abs(y) < grid_Ry / 2):  # check if it is in grid area
+                    trans_opt.arTr[pointer] = 0
+                    trans_opt.arTr[pointer + 1] = 0
+
+                    # Shift the pointer by 2.
+            pointer += 2
+
+            # Step x by _hx.
+            x += _hx
+
+        # Step y by _hy.
+        y += _hy
+
+    return trans_opt
+
+#****************************************************************************
 def srwl_opt_setup_surf_height_1d(_height_prof_data, _dim, _ang, _ang_r=0, _amp_coef=1, _ar_arg_long=None, _nx=0, _ny=0, _size_x=0, _size_y=0, _xc=0, _yc=0):
     """
     Setup Transmission type optical element with 1D (mirror or grating) surface Heght Profile data
