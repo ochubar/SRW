2ace71efad26ee539fd111f4d4aeed10744f5444
Maksim Rakitin <mrakitin@bnl.gov>
2021-10-10 17:39:39 -0400

    TST: add test for example01

---
+++ python/srwpy/tests/conftest.py
@@ -0,0 +1,16 @@
+import os
+import pytest
+
+
+@pytest.fixture
+def examples_dir():
+    _examples_dir = os.path.abspath(os.path.join(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.dirname(
+                    os.path.dirname(__file__)
+                )
+            )
+        ), "examples", "python")
+    )
+    return _examples_dir
