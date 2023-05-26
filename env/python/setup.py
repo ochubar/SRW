4fe7bce10fd8a94fed9cb0a353035ae5dc9e2db4
Max Rakitin <mrakitin@bnl.gov>
2023-05-26 02:34:37 -0400

    Use `README.md` for long description without copying it to `env/python/`
    Copying is not needed as we do not generate sdist archives, and wheels
    deal with the metadata differently, so it's fine to read the file from
    two levels above. That will also simplify local installs from the cloned
    repo and in the conda-forge feedstock.

--- env/python/setup.py
+++ env/python/setup.py
@@ -77,8 +77,8 @@ class CMakeBuild(build_ext):
 base_dir = os.path.dirname(os.path.realpath(__file__))
 original_src_dir = os.path.join(base_dir, '../..')
 
-#Read README.md for long_description
-long_description = open(os.path.join(base_dir, 'README.md')).read()
+# Read README.md for long_description
+long_description = open(os.path.join(original_src_dir, 'README.md')).read()
 
 with open(os.path.join(base_dir, 'requirements.txt')) as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
